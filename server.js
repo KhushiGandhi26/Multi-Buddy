b098b6efaac2f85b04bbd391e5c721b243db55d9
Khushi Gandhi <khushigandhi683@gmail.com>
2023-06-09 20:47:39 +0530

    Updated files

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+// const errorHandler = require("./middelwares/errorMiddleware");
+
+dotenv.config();
+
+
+//mongo connection
+connectDB();
+
+
+//Rest Object
+const app=express(); 
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+// app.use(errorHandler);
+
+
+const PORT=process.env.PORT || 8080 ;
+
+app.listen(PORT,()=>{
+    console.log(`Server Running in ${process.env.DEV_MODE} on ${PORT} port`.bgCyan.white);
+});
