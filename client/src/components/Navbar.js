f1c8f72c9418a9298a9cbd657ae4225d538ef5da
Khushi Gandhi <khushigandhi683@gmail.com>
2023-06-10 21:51:52 +0530

    .

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import {Box,Link,Typography, colors} from '@mui/material'
+import {NavLink} from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <div>
-        <h1>Nav Menu</h1>
-    </div>
+    <Box width={'100%'} 
+    p="1rem 6%" 
+    textAlign={'center'} 
+    sx={{boxShadow:3,mb:2 ,bgcolor:'purple',color:'white'}}>
+      <Typography variant="h1" color={"white"} fontWeight="bold">
+        AI CPT3
+      </Typography>
+      <NavLink to='/register' p={1}>Sign Up</NavLink>
+      <NavLink href='/login' p={1}>Sign In</NavLink>
+    </Box>
   )
 }
 
