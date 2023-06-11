b5b5d8eec5083a79fb9dc4e0c2f083efa1269d9c
Khushi Gandhi <khushigandhi683@gmail.com>
2023-06-11 12:08:41 +0530

    created theme.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
 import {Routes,Route} from 'react-router-dom'
+import { useMemo } from 'react';
+import { createTheme } from '@mui/material';
+import { themeSettings } from './theme';
+import CssBaseline from '@mui/material';
 import Navbar from './components/Navbar';
 import Homepage from './pages/Homepage';
 import Register from './pages/Register';
 import Login from './pages/Login';
 
 function App() {
+  const theme =useMemo(()=>c)
   return (
     <>
       <Navbar/>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import {Box,Link,Typography, colors} from '@mui/material'
 import {NavLink} from "react-router-dom"
+import { useTheme } from '@emotion/react'
 
 const Navbar = () => {
+  const theme =useTheme();
   return (
     <Box width={'100%'} 
+    backgroundColor={theme.palette.background.alt}
     p="1rem 6%" 
     textAlign={'center'} 
     sx={{boxShadow:3,mb:2 ,bgcolor:'purple',color:'white'}}>
       <Typography variant="h1" color={"white"} fontWeight="bold">
         AI CPT3
       </Typography>
-      <NavLink to='/register' p={1}>Sign Up</NavLink>
-      <NavLink href='/login' p={1}>Sign In</NavLink>
+      <NavLink to="/register" p={1}>Sign Up</NavLink>
+      <NavLink href="/login" p={1}>Sign In</NavLink>
     </Box>
   )
 }
