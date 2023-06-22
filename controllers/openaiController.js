79586557ca2f9015a4b1b12f63cbe17812f38113
Khushi Gandhi <khushigandhi683@gmail.com>
2023-06-22 15:14:04 +0530

    .

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const { Configuration, OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi } = require("openai")
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
